4aeeb332289116818bbe61e921d1c3ee08d2bfbb
NadaComsci <34049504+NadaComsci@users.noreply.github.com>
2019-02-05 13:58:31 +0700

    [Edited]: Report Student
    - หน้าจอเพิ่ม parameter 2 ตัว คือ department, education
    - เพิ่ม parameter ในการเรียกแสดงรายงาน
    - เพิ่ม parameter ในการ download ข้อมูล

--- kmutt-excel-report-front-portlet/docroot/controller/cGraduateStudentPercentReport.js
+++ kmutt-excel-report-front-portlet/docroot/controller/cGraduateStudentPercentReport.js
@@ -1,5 +1,7 @@
 var restfulPathDropDownYear = apiURL+"/GraduateStudent/YearList";
 var restfulPathDropDownFaculty = apiURL+"/GraduateStudent/FacultyList";
+var restfulPathDropDownDepartment=apiURL+"/GraduateStudent/DepartmentList";
+var restfulPathDropDownEducation=apiURL+"/GraduateStudent/EducationList";
 var restfulPathImportExcel = apiURL+"/GraduateStudent/Import/Percent";
 var restfulPathExportExcel = apiURL+"/GraduateStudent/Export/Percent";
 
@@ -10,17 +12,22 @@ var gerReportFn = function() {
 	 
 	var param_year= $("#param_year").val();
 	var param_faculty= $("#param_faculty").val();
+	var param_department= $("#param_department").val();
+	var param_education= $("#param_education").val();
 	var param_type = $("#param_type").val();
 	var parameter = {};
 	var template_name ="";
 	
 	parameter = {
 			param_year:param_year,
-			param_faculty_name:param_faculty
+			param_faculty_name:param_faculty,
+			param_department_name:param_department,
+			param_education:param_education,
+			param_type:param_type
 	};
 	
 	  var data = JSON.stringify(parameter);
-	  var url_report_jasper = apiURL+"/generate?template_name=graduate_student_percent_report&template_format="+param_type+"&used_connection=1&inline=1&data="+data;
+	  var url_report_jasper = apiURL+"/generate?template_name=graduate_student_percent_report&template_format="+param_type+"&used_connection=1&inline=1&data="+data+"&subreport_bundle=1";
 	  
 	  console.log(url_report_jasper);
 	  
@@ -64,6 +71,51 @@ var dropDownListFaculty = function(){
 					html+="<option  value='"+indexEntry["faculty_name"]+"'>"+indexEntry["faculty_name"]+"</option>";	
 			});
 			$("#param_faculty").html(html);
+			dropDownListDepartment();
+		}
+	});	
+};
+
+var dropDownListDepartment = function(){
+	var html="";
+	$.ajax ({
+		url:restfulPathDropDownDepartment,
+		type:"get" ,
+		data: { 
+			param_year:$("#param_year").val(),
+			param_faculty:$("#param_faculty").val()
+		},
+		dataType:"json" ,
+		async:true,
+		success:function(data){
+			html+="<option value=''>All Department Name</option>";
+			$.each(data,function(index,indexEntry){
+				html+="<option  value='"+indexEntry["department_name"]+"'>"+indexEntry["department_name"]+"</option>";	
+			});
+			$("#param_department").html(html);
+			dropDownListEducation();
+		}
+	});	
+};
+
+var dropDownListEducation = function(){
+	var html="";
+	$.ajax ({
+		url:restfulPathDropDownEducation,
+		type:"get" ,
+		data: { 
+			param_year:$("#param_year").val(),
+			param_faculty:$("#param_faculty").val(),
+			param_department:$("#param_department").val()
+		},
+		dataType:"json" ,
+		async:true,
+		success:function(data){
+			html+="<option value='0'>All Education Name</option>";
+			$.each(data,function(index,indexEntry){
+					html+="<option  value='"+indexEntry["education_id"]+"'>"+indexEntry["education_name"]+"</option>";	
+			});
+			$("#param_education").html(html);
 		}
 	});	
 };
@@ -79,16 +131,12 @@ $(document).ready(function() {
           html: true
       });
       
-/*      
-	$("#exportToExcel").click(function(){
-		$("form#formExportToExcel").attr("action",$("#url_portlet").val()+"/file/import-honor-bachelor-template.xlsx");
-	});
-*/
-      
       $("#exportToExcel").click(function(){
     		var param="";
     		param+="param_year="+$("#param_year").val();
     		param+="&param_faculty="+$("#param_faculty").val();
+    		param+="&param_department="+$("#param_department").val();
+    		param+="&param_education="+$("#param_education").val();
 
     		$("form#formExportToExcel").attr("action",restfulPathExportExcel+"?"+param);
     	});
@@ -99,7 +147,12 @@ $(document).ready(function() {
 	$("#param_year").change(function () {
 		dropDownListFaculty();
 	});
-	
+	$("#param_faculty").change(function () {
+		dropDownListDepartment();
+	});
+	$("#param_department").change(function () {
+		dropDownListEducation();
+	});
 	$("#btnSearchAdvance").click(function () {
 		gerReportFn();
 	});
